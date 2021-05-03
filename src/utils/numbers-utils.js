194964392c47fe567893060be762370539517ac2
cat-in-magic-hat <39413869+cat-in-magic-hat@users.noreply.github.com>
2021-05-03 18:49:19 +0300

    remove extra code

--- src/utils/numbers-utils.js
+++ src/utils/numbers-utils.js
@@ -1,8 +1,5 @@
 'use strict';
 
 exports.getDigitsCount = (value) => {
-  if (!Number.isInteger(value)) {
-    throw new Error(`Non integer values are not supported.`);
-  }
   return Math.floor(Math.log10(Math.abs(value))) + 1;
 };
